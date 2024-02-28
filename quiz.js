3fe5700755922c627feb0f9128daccfe5d26e723
thor-51 <111634273+thor-51@users.noreply.github.com>
2024-02-28 22:50:36 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+        ]
+    },
+
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+        ] 
+    },
+
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+        ]
+    },
+
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+            {text:'Shark', correct: false},
+        ]
+    }
+
+];
